89c0616714d1e12f663d32b5fb1bcb90e7ea9b96
Juan Ludevid <jmludevid@hotmail.com>
2024-02-07 17:00:00 +1100

    setting up GlobalStyles.js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,27 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+`;
 export default function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+      </StyledApp>
+    </>
+  );
 }
